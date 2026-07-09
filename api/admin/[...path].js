4db1fe38442cef3396720412c19a2c5e8b03350b
shazoxalik-ctrl <shazoxalik@gmail.com>
2026-07-09 13:16:42 +0500

    debug: log path resolution

--- api/admin/[...path].js
+++ api/admin/[...path].js
@@ -4,5 +4,6 @@ export const config = { api: { bodyParser: false } };
 
 export default function handler(req, res) {
   const path = Array.isArray(req.query.path) ? req.query.path : [req.query.path].filter(Boolean);
+  console.log('admin catch-all debug:', req.method, req.url, JSON.stringify(req.query), JSON.stringify(path));
   return adminHandler(req, res, path);
 }
